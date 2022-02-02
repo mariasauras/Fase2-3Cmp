6cc17fa18bdfe3a175909d6c1e849d205c2a64b4
mariasauras <saurasm02@gmail.com>
2022-02-02 10:15:14 +0000

    Improve test and finish

--- ex_entrada.jl
+++ ex_entrada.jl
@@ -5,11 +5,14 @@ end
 function mean(a::Float32, b::Float32)::Float32
    return (a+b)/2
 end
+function readValue(v::Vector{Int32})
+   temp = v[1]
+end
 m = 2
 k = sum(5, m) * 5
 matrix = [1 4;2 5;3 5]
 matrix[m,1]
 vector = [1 4 3]
-vector[1]
+readValue(vector)
 x = 17.0
 z = x + mean(x-7, vector[1]*2)
